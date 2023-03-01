dca2a36885c435b8ddf462e72e537714a1efedb3
Minh Nghia <nghia.nguyenminh@powergatesoftware.com>
2023-03-01 10:12:03 +0700

    add lodash

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { isEmpty } from 'lodash';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import './App.css';
 
@@ -7,41 +9,50 @@ const BASE_URL = 'http://11.30.11.215:3000/account/authen';
 const authenticateStep1 = `${BASE_URL}?t=1`;
 const authenticateStep2 = `${BASE_URL}?t=2`;
 
+const AUTHENTICATE_KEY_1 = 'qrCode';
+const AUTHENTICATE_KEY_2 = 'confirm';
+
 function App() {
-  const {
-    isLoading,
-    error,
-    data: qrCode,
-  } = useQuery('qrCode', () => axios.get(authenticateStep1).then(({ data }) => data));
+  const [qrCode, setQrCode] = useState();
+  const [error, setError] = useState();
+  const [userInfo, setUserInfo] = useState();
 
-  const { data: userInfo } = useQuery('confirm', () => axios.get(authenticateStep2).then(({ data }) => data));
+  useQuery(AUTHENTICATE_KEY_1, () => axios.get(authenticateStep1).then(({ data }) => data), {
+    enabled: isEmpty(qrCode),
+    onSuccess: setQrCode,
+    onError: setError,
+  });
 
-  if (!!userInfo) {
-    return (
-      <div>
-        <img src={userInfo.avatar} alt="user name" />
-        <span>{userInfo.display_name}</span>
-      </div>
-    );
-  }
+  useQuery(AUTHENTICATE_KEY_2, () => axios.get(authenticateStep2).then(({ data }) => data), {
+    enabled: isEmpty(userInfo),
+    onSuccess: setUserInfo,
+    onError: setError,
+  });
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>Zalo Me</h2>
-        {isLoading ? (
-          <h2>Loading...</h2>
-        ) : !!error ? (
-          <p>Error: {error.message}</p>
+        {!!userInfo ? (
+          <>
+            <img src={userInfo?.avatar} alt="user name" />
+            <span>{userInfo?.display_name}</span>
+          </>
         ) : (
           <>
-            <p>Đăng nhập tài khoản Zalo để kết nối với ứng dụng Zalo Web</p>
-            <div className="qrCode">
-              <img src={qrCode.image} alt="qrCode" />
-            </div>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-              Quét mã QR bằng Zalo để đăng nhập
-            </a>
+            {!!error ? (
+              <p>Error: {error.message}</p>
+            ) : (
+              <>
+                <p>Đăng nhập tài khoản Zalo để kết nối với ứng dụng Zalo Web</p>
+                <div className="qrCode">
+                  <img src={qrCode?.image} alt="qrCode" />
+                </div>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                  Quét mã QR bằng Zalo để đăng nhập
+                </a>
+              </>
+            )}
           </>
         )}
       </header>
